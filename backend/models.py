c6fa9b778f0e4fb1cf9350f6c4054580e4116d25
ishan <ishan@velou.com>
2024-05-04 21:42:24 +0530

    Sqlite database connected

---
+++ backend/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_superuser = Column(Boolean, default=False)

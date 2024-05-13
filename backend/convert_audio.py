9ca30e073c858100a55b22fa63b9034a9b17d73c
nipun thejan <76776959+nipun-thejan@users.noreply.github.com>
2024-05-14 00:17:07 +0530

    convert audio wav added

---
+++ backend/convert_audio.py
@@ -0,0 +1,10 @@
+from pydub import AudioSegment
+
+def webm_to_wav(webm_file_path="backend/uploads/uprecorded_audio.webm", save_wav_file_name="backend/uploads/recorded_audio_wav.wav"):
+    audio = AudioSegment.from_file(webm_file_path)
+
+    audio.export(save_wav_file_name, format="wav")
+    print("Audio conversion completed successfully!")
+
+if __name__ == "__main__":
+    webm_to_wav()

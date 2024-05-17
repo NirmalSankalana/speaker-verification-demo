19c1d0fd865160c48a34e53a68eec42c9c0dff61
nipun thejan <76776959+nipun-thejan@users.noreply.github.com>
2024-05-17 16:35:29 +0530

    gen emb

--- backend/generate_embedding.py
+++ backend/generate_embedding.py
@@ -1,8 +1,11 @@
 from verifyvoice import ModelLoader
 
+class Model():
+    def __init__(self, model_name="WavLM", attention_heads=8):
+        self.model = ModelLoader(model_name="WavLM", attention_heads=attention_heads)
+        
 
-def get_embedding(audio_path, attention_heads=8):
-    model = ModelLoader(model_name="WavLM", attention_heads=8)
-    emb = model.get_embedding(audio_path)
-    print(f"embedding generated for audio : {audio_path} shape {emb.shape}")
-    return emb
+    def get_embedding(self, audio_path):
+        emb = self.model.get_embedding(audio_path)
+        print(f"embedding generated for audio : {audio_path} shape {emb.shape}")
+        return emb
